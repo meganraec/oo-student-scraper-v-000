f93ea2fbadbe8b5f9ad574299c82610ca76676ea
Megan Collier <meganraec@live.com>
2018-11-27 19:39:41 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,7 +14,7 @@ class Scraper
       scraped_students << {
         :name => card.css("h4.student-name").text,
         :location => card.css("p.student-location").text,
-        :profile_url => ("./fixtures/student-site") + card.css("a").attribute('href').value
+        :profile_url => ("./fixtures/student-site") + card.css("a").attribute("href").value
       }
       end
     scraped_students
