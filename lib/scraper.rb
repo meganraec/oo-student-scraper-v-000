1cb910b84a8d5cbc1c20d950b824618126b5b967
Megan Collier <meganraec@live.com>
2018-11-27 18:59:41 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,41 @@
 require 'open-uri'
 require 'pry'
+require 'nokogiri'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+# is a class method that scrapes the student index page ('./fixtures/student-site/index.html') and a returns an array of hashes in which each hash represents one student
+    index = Nokogiri::HTML(open(index_url))
+    scraped_students = []
+    cards = index.css('div.student-card')
+
+    cards.collect do |card|
+      scraped_students << {
+        :name => card.css('h4.student-name').text,
+        :location => card.css('p.student-location').text,
+        :profile_url => ('./fixtures/student-site') + card.css('a').attribute('href').value
+      }
+      end
+    scraped_students
   end
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+# is a class method that scrapes a student's profile page and returns a hash of attributes describing an individual student
+# can handle profile pages without all of the social links
+
+    profile = Nokogiri::HTML(open(profile_url))
+    attributes = {}
+
+    profile.css('social-icon-container')
+end
+  #
+  #   :twitter =>
+  #   :linkedin =>
+  #   :github =>
+  #   :blog =>
+  #   :profile_quote =>
+  #   :bio => profile.css('div.description-holder p').text
+  # end
 
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,24 @@
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
 
   @@all = []
 
   def initialize(student_hash)
-    
+# takes in an argument of a hash and sets that new student's attributes using the key/value pairs of that hash.
+# adds that new student to the Student class' collection of all existing students, stored in the `@@all` class variable.
   end
 
   def self.create_from_collection(students_array)
-    
+# uses the Scraper class to create new students with the correct name and location.
   end
 
   def add_student_attributes(attributes_hash)
-    
+# uses the Scraper class to get a hash of a given students attributes and uses that hash to set additional attributes for that student.
   end
 
   def self.all
-    
+# returns the class variable @@all
+    @@all
   end
 end
-
